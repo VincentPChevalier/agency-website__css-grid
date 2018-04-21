e2043f4f494580d4240ff7098ed65b8afde6ecfa
Vincent Chevalier <info@vincentpchevalier.com>
2018-04-21 16:35:16 -0400

    styled overlay menu and typography, main page and global nav styles

--- dev/scripts/script.js
+++ dev/scripts/script.js
@@ -1,7 +1,17 @@
 const hamburger = document.querySelector('.hamburger');
+const overlay = document.querySelector('.menu-overlay');
+const menuExit = document.querySelector('.menu-exit');
+const body = document.querySelector('body')
 
-hamburger.addEventListener("click", function(e) {
-	hamburger.classList.add("is-active")
+hamburger.addEventListener('click', function(e) {
+	hamburger.classList.toggle("is-active");
+	overlay.classList.remove('hidden');
+	body.classList.toggle('noscroll');
+});
+
+menuExit.addEventListener('click', function() {
+	overlay.classList.toggle('hidden');
+	body.classList.toggle('noscroll');
 });
 
 // add X into DOM next to menu
